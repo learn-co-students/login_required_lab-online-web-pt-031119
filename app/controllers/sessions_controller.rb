bf240f458fe7ee36d00723a45f2700f167a3edb1
“Mark <mkanko66@gmail.com>
2019-08-19 21:36:07 -0700

    completed login_required_lab

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+    
+    def new
+    end 
+
+    def create
+        #binding.pry 
+        if params[:name].nil?
+          redirect_to '/login'
+        elsif params[:name] == ""
+          redirect_to '/login' 
+        else    
+          session[:name] = params[:name]
+          #binding.pry
+          render :home 
+        end  
+      end
+      
+      def destroy 
+        session.delete :name 
+        redirect_to '/login'
+      end
+end 
