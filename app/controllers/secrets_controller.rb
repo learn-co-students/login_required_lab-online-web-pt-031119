a1ed64f8c988e86b252fba3431d464b797b83b29
Danieleclima <daniel.echuaca.lima@gmail.com>
2019-09-09 22:08:14 +0100

    Done.

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -2,7 +2,7 @@ class SecretsController < ApplicationController
  
     def show
         if !current_user
-            redirect_to '/'
+            redirect_to '/sessions/new'
         end
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,8 +5,14 @@ class SessionsController < ApplicationController
     end
 
     def create
+        if !params[:name] || params[:name].empty?
+            redirect_to '/sessions/new'
+        else
+            session[:name] = params[:name]
+        end
     end
 
     def destroy
+        session.delete :name
     end
 end
